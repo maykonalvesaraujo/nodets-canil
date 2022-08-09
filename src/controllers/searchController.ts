90d52a58e949329b1033038f0c301e5511f265d7
maykon <nokiam13@hotmail.com>
2022-08-09 17:47:14 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,18 @@
 import { Request, Response} from 'express';
+import {Pet} from '../models/Pet';
+import {createMenuObject} from '../helpers/createMenuObject';
 
 export const search = (req: Request, res: Response)=>{
+    let query: string = req.query.q as string;
 
-    //res.send('pages/search);
+    if(! query){
+        res.redirect('/');
+        return;
+    }
+    let list = Pet.getFromName(query);
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 };
